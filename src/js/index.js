7000d75cde86b85613f6997f3531c61a8aa6a7bc
Ahmed-202020 <ak812554@gmail.com>
2022-02-13 14:27:38 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,49 @@
 $(function(){
-   $(".indecators li").on("click" , function(e){
-      $(".indecators li").removeClass("active") ; 
-      $(this).addClass("active");
-   });
-   $(".en").on("click" , function(){
-      $("html").attr("dir" , "ltr") ;
-   }) ; 
-   $(".ar").on("click" , function(){
-      $("html").attr("dir" , "rtl") ;
-   }) ; 
+    $(".indecators li").on("click" , function(e){
+        $(".indecators li").removeClass("active") ; 
+        $(this).addClass("active");
+    });
+    $(".en").on("click" , function(){
+        $("html").attr("dir" , "ltr") ;
+    }) ; 
+    $(".ar").on("click" , function(){
+        $("html").attr("dir" , "rtl") ;
+    }) ; 
 
-   function screen_slider() {
-      var owl = $(".screen-slider");
-      owl.owlCarousel({
-          loop: true,
-          margin: 30,
-          navigation: true,
-          items: 5,
-          smartSpeed: 1000,
-          dots: true,
-          autoplay: true,
-          center: true,
-          autoplayTimeout: 2000,
-          dotsEach: true,
-          responsive: {
-              0: {
-                  items: 1
-              },
-              480: {
-                  items: 1
-              },
-              767: {
-                  items: 3
-              },
-              992: {
-                  items: 5
-              },
-              1920: {
-                  items: 5
-              }
-          }
-      });
-  }
-  screen_slider();
+    function screen_slider() {
+        var owl = $(".screen-slider");
+        owl.owlCarousel({
+            loop: true,
+            margin: 30,
+            navigation: true,
+            items: 5,
+            smartSpeed: 1000,
+            dots: true,
+            autoplay: true,
+            center: true,
+            autoplayTimeout: 2000,
+            dotsEach: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                767: {
+                    items: 3
+                },
+                992: {
+                    items: 5
+                },
+                1920: {
+                    items: 5
+                }
+            }
+        });
+    }
+    screen_slider();
 });
+
+new WOW().init() ; 
 
